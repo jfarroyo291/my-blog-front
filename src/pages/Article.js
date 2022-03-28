c0287b62c33143e3516a70b4a17cbddc419861c1
jfarroyo291 <j-a-m291@hotmail.com>
2022-03-28 10:29:40 -0500

    modified files

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import articleContent from './article-content';
 import Articles from '../components/Articles';
 import NotFound from './NotFound';
+import CommentsLists from '../components/CommentsLists';
+import AddCommentForm from '../components/AddCommentForm';
 
 const Article = () => {
     const { name } = useParams();
     const article = articleContent.find((article) => article.name === name);
+
+    const [articleInfo, setArticleInfo] = useState({comments: []});
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        };
+        fetchData();
+    }, [name]);
+
     if(!article) return <NotFound />;
     const otherArticles = articleContent.filter((article) => article.name !== name);
     return (
@@ -19,6 +34,8 @@ const Article = () => {
                     {paragraph}
                 </p>
             ))}
+            <CommentsLists comments = {articleInfo.comments} />
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
             <h1 className="sm:text-2x text-xl font-bold mt-4 mb-4 text-gray-900">
                 Other Articles
             </h1>
